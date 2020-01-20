b7725b98036452cc4cb0bbcce46c3dba35dc7146
MaZderMind <git@mazdermind.de>
2020-01-20 22:50:13 +0100

    minimal ledserver connection

--- software/jukebox/leds.py
+++ software/jukebox/leds.py
@@ -1,9 +1,23 @@
+import socket
+
+SHOW_STRIP0_IMMEDIATE = b'\x11'
+
+CMD_BLACKOUT = b'\x00'
+
+LED_SERVER_IP = "::1"
+LED_SERVER_PORT = 5555
+
+
 class Leds(object):
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        self.sock.connect((LED_SERVER_IP, LED_SERVER_PORT))
+
     def blackout(self):
-        pass
+        self.sock.send(CMD_BLACKOUT)
 
     def idle(self):
-        pass
+        self.sock.send(SHOW_STRIP0_IMMEDIATE + b'\xFF\x00\x00')
 
     def show(self, key_combo):
-        pass
+        self.sock.send(SHOW_STRIP0_IMMEDIATE + b'\x00\xFF\x00')
