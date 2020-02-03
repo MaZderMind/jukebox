ac0728016d98918aa200e9b1ee402c7ffdadd0e2
Peter Körner <pkoerner@seibert-media.net>
2020-02-03 22:20:24 +0100

    simplify calculation by staing in [0:1] right until the end

--- software/animations/color_utils.py
+++ software/animations/color_utils.py
@@ -1,7 +1,7 @@
-from collections import Iterable
+from collections.abc import Iterable
 
 
-def all_channels(func):
+def for_each_channel(func):
     def wrapper(channels, *args, **kwargs):
         if isinstance(channels, Iterable):
             # print("is tuple", channels)
@@ -16,39 +16,29 @@ def all_channels(func):
     return wrapper
 
 
-@all_channels
-def to_ff(x):
+def clamp(v, v_min=0.0, v_max=1.0):
+    return max(min(float(v), v_max), v_min)
+
+
+@for_each_channel
+def to_byte(v):
     """ Returns a sRGB value in the range [0,255]
         for linear input in [0,1]
         Can be called with a single Value or with an (R,G,B)-Tuple
     """
-    return min(int(256 * x), 255)
+    return int(clamp(v) * 255)
 
 
-@all_channels
-def from_ff(x):
+@for_each_channel
+def from_ff(v):
     """ Returns a sRGB value in the range [0,255]
         for linear input in [0,1]
         Can be called with a single Value or with an (R,G,B)-Tuple
     """
-    return x / 255.0
+    return clamp(v / 255)
 
 
-@all_channels
-def from_srgb(x):
-    """ Returns a linear value in the range [0,1]
-        for sRGB input in [0,255].
-        Can be called with a single Value or with an (R,G,B)-Tuple
-    """
-    x /= 255.0
-    if x <= 0.04045:
-        y = x / 12.92
-    else:
-        y = ((x + 0.055) / 1.055) ** 2.4
-    return y
-
-
-def all_channels_two_colors(func):
+def for_each_channel_two_args(func):
     def wrapper(channels1, channels2, *args, **kwargs):
         if isinstance(channels1, tuple) and isinstance(channels2, tuple):
             return tuple(
@@ -61,19 +51,14 @@ def all_channels_two_colors(func):
     return wrapper
 
 
-@all_channels_two_colors
+@for_each_channel_two_args
 def linear_interpolate(color1, color2, frac):
     return color1 * (1 - frac) + color2 * frac
 
 
-def steps(color1, color2, steps):
-    color1_01 = from_ff(color1)
-    color2_01 = from_ff(color2)
-
+def steps_two_colors(color1, color2, steps):
     return [
-        to_ff(
-            linear_interpolate(color1_01, color2_01, step / (steps - 1))
-        )
+        linear_interpolate(color1, color2, step / (steps - 1))
         for step in range(steps)
     ]
 
@@ -86,7 +71,7 @@ def rgb_gamma(color):
     b_correction = 0.8
 
     return (
-        int((color[0] / 255) ** (1 / gamma) * r_correction * 255),
-        int((color[1] / 255) ** (1 / gamma) * g_correction * 255),
-        int((color[2] / 255) ** (1 / gamma) * b_correction * 255)
+        color[0] ** (1 / gamma) * r_correction,
+        color[1] ** (1 / gamma) * g_correction,
+        color[2] ** (1 / gamma) * b_correction,
     )
--- software/animations/test.py
+++ software/animations/test.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 import time
 
-from color_utils import steps, rgb_gamma
+from color_utils import steps_two_colors, rgb_gamma, to_byte
 from reverse_repeat import ReverseRepeatIterator
 from send import send
 
-c1 = (255, 0, 0)
-c2 = (0, 0, 255)
+c1 = (1., 0., 1.)
+c2 = (0., 0., 0.)
 
 rows = 15
 leds_per_row = 11
-color_steps = steps(c1, c2, 30)
+color_steps = steps_two_colors(c1, c2, 30)
 repeating_iterator = ReverseRepeatIterator(color_steps)
 
 for colorset in repeating_iterator:
     msg = b''
     for _ in range(rows):
         for color in colorset[0:leds_per_row]:
-            msg += bytes(rgb_gamma(color))
+            msg += bytes(to_byte(rgb_gamma(color)))
 
     send(msg)
     time.sleep(1 / 30)
