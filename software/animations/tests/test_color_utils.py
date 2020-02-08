6d56f99a521a9cedc6126d55b0dba08225066c61
Peter Körner <pkoerner@seibert-media.net>
2020-02-08 20:16:01 +0100

    convert to pytest

--- software/animations/tests/test_color_utils.py
+++ software/animations/tests/test_color_utils.py
@@ -1,29 +1,24 @@
-from unittest import TestCase
-
 from color_utils import to_byte, clamp
 
 
-class Test(TestCase):
-    def test_to_byte(self):
-        assert to_byte(-1.) == 0
-        assert to_byte(0.) == 0
-        assert to_byte(0.25) == 63
-        assert to_byte(0.5) == 127
-        assert to_byte(0.75) == 191
-        assert to_byte(1.0) == 255
-        assert to_byte(1.5) == 255
+def test_to_byte():
+    assert to_byte(-1.) == 0
+    assert to_byte(0.) == 0
+    assert to_byte(0.25) == 63
+    assert to_byte(0.5) == 127
+    assert to_byte(0.75) == 191
+    assert to_byte(1.0) == 255
+    assert to_byte(1.5) == 255
 
-    def test_clamp(self):
-        assert clamp(-10000) == 0.
-        assert clamp(-1) == 0.
-        assert clamp(-0.1) == 0.
-        assert clamp(0.) == 0.
-        assert clamp(0.25) == .25
-        assert clamp(0.5) == .5
-        assert clamp(0.75) == .75
-        assert clamp(1.) == 1.
-        assert clamp(1.5) == 1.
-        assert clamp(100000.) == 1.
 
-    def test_generate_color_steps(self):
-        self.fail()
+def test_clamp():
+    assert clamp(-10000) == 0.
+    assert clamp(-1) == 0.
+    assert clamp(-0.1) == 0.
+    assert clamp(0.) == 0.
+    assert clamp(0.25) == .25
+    assert clamp(0.5) == .5
+    assert clamp(0.75) == .75
+    assert clamp(1.) == 1.
+    assert clamp(1.5) == 1.
+    assert clamp(100000.) == 1.
--- software/animations/tests/test_reverse_repeat.py
+++ software/animations/tests/test_reverse_repeat.py
@@ -1,74 +1,74 @@
-from unittest import TestCase
-
-from reverse_repeat import ReverseRepeatIterator
 from direction import Direction
+from reverse_repeat import ReverseRepeatIterator
 
 
-class TestReverseRepeatIterator(TestCase):
-    def testThreeItemsForward(self):
-        iterator = ReverseRepeatIterator(['A', 'B', 'C'], direction=Direction.FORWARD)
+def test_three_items_forward():
+    iterator = ReverseRepeatIterator(['A', 'B', 'C'], direction=Direction.FORWARD)
 
-        for _ in range(3):
-            assert next(iterator) == ['A', 'B', 'C']
+    for _ in range(3):
+        assert next(iterator) == ['A', 'B', 'C']
 
-            assert next(iterator) == ['B', 'A', 'B']
-            assert next(iterator) == ['C', 'B', 'A']
-            assert next(iterator) == ['B', 'C', 'B']
+        assert next(iterator) == ['B', 'A', 'B']
+        assert next(iterator) == ['C', 'B', 'A']
+        assert next(iterator) == ['B', 'C', 'B']
 
-            assert next(iterator) == ['A', 'B', 'C']
+        assert next(iterator) == ['A', 'B', 'C']
 
-            assert next(iterator) == ['B', 'A', 'B']
-            assert next(iterator) == ['C', 'B', 'A']
-            assert next(iterator) == ['B', 'C', 'B']
+        assert next(iterator) == ['B', 'A', 'B']
+        assert next(iterator) == ['C', 'B', 'A']
+        assert next(iterator) == ['B', 'C', 'B']
 
-    def testThreeItemsReverse(self):
-        iterator = ReverseRepeatIterator(['A', 'B', 'C'], direction=Direction.REVERSE)
 
-        for _ in range(3):
-            assert next(iterator) == ['A', 'B', 'C']
+def test_three_items_reverse():
+    iterator = ReverseRepeatIterator(['A', 'B', 'C'], direction=Direction.REVERSE)
 
-            assert next(iterator) == ['B', 'C', 'B']
-            assert next(iterator) == ['C', 'B', 'A']
-            assert next(iterator) == ['B', 'A', 'B']
+    for _ in range(3):
+        assert next(iterator) == ['A', 'B', 'C']
 
-            assert next(iterator) == ['A', 'B', 'C']
+        assert next(iterator) == ['B', 'C', 'B']
+        assert next(iterator) == ['C', 'B', 'A']
+        assert next(iterator) == ['B', 'A', 'B']
 
-            assert next(iterator) == ['B', 'C', 'B']
-            assert next(iterator) == ['C', 'B', 'A']
-            assert next(iterator) == ['B', 'A', 'B']
+        assert next(iterator) == ['A', 'B', 'C']
 
-    def testFiveItemsForward(self):
-        iterator = ReverseRepeatIterator(['A', 'B', 'C', 'D', 'E', 'F'], direction=Direction.FORWARD)
+        assert next(iterator) == ['B', 'C', 'B']
+        assert next(iterator) == ['C', 'B', 'A']
+        assert next(iterator) == ['B', 'A', 'B']
 
-        for _ in range(3):
-            assert next(iterator) == ['A', 'B', 'C', 'D', 'E', 'F']
 
-            assert next(iterator) == ['B', 'A', 'B', 'C', 'D', 'E']
-            assert next(iterator) == ['C', 'B', 'A', 'B', 'C', 'D']
-            assert next(iterator) == ['D', 'C', 'B', 'A', 'B', 'C']
-            assert next(iterator) == ['E', 'D', 'C', 'B', 'A', 'B']
+def test_five_items_forward():
+    iterator = ReverseRepeatIterator(['A', 'B', 'C', 'D', 'E', 'F'], direction=Direction.FORWARD)
 
-            assert next(iterator) == ['F', 'E', 'D', 'C', 'B', 'A']
+    for _ in range(3):
+        assert next(iterator) == ['A', 'B', 'C', 'D', 'E', 'F']
 
-            assert next(iterator) == ['E', 'F', 'E', 'D', 'C', 'B']
-            assert next(iterator) == ['D', 'E', 'F', 'E', 'D', 'C']
-            assert next(iterator) == ['C', 'D', 'E', 'F', 'E', 'D']
-            assert next(iterator) == ['B', 'C', 'D', 'E', 'F', 'E']
+        assert next(iterator) == ['B', 'A', 'B', 'C', 'D', 'E']
+        assert next(iterator) == ['C', 'B', 'A', 'B', 'C', 'D']
+        assert next(iterator) == ['D', 'C', 'B', 'A', 'B', 'C']
+        assert next(iterator) == ['E', 'D', 'C', 'B', 'A', 'B']
 
-    def testFiveItemsReverse(self):
-        iterator = ReverseRepeatIterator(['A', 'B', 'C', 'D', 'E', 'F'], direction=Direction.REVERSE)
+        assert next(iterator) == ['F', 'E', 'D', 'C', 'B', 'A']
 
-        for _ in range(3):
-            assert next(iterator) == ['A', 'B', 'C', 'D', 'E', 'F']
+        assert next(iterator) == ['E', 'F', 'E', 'D', 'C', 'B']
+        assert next(iterator) == ['D', 'E', 'F', 'E', 'D', 'C']
+        assert next(iterator) == ['C', 'D', 'E', 'F', 'E', 'D']
+        assert next(iterator) == ['B', 'C', 'D', 'E', 'F', 'E']
 
-            assert next(iterator) == ['B', 'C', 'D', 'E', 'F', 'E']
-            assert next(iterator) == ['C', 'D', 'E', 'F', 'E', 'D']
-            assert next(iterator) == ['D', 'E', 'F', 'E', 'D', 'C']
-            assert next(iterator) == ['E', 'F', 'E', 'D', 'C', 'B']
 
-            assert next(iterator) == ['F', 'E', 'D', 'C', 'B', 'A']
+def test_five_items_reverse():
+    iterator = ReverseRepeatIterator(['A', 'B', 'C', 'D', 'E', 'F'], direction=Direction.REVERSE)
 
-            assert next(iterator) == ['E', 'D', 'C', 'B', 'A', 'B']
-            assert next(iterator) == ['D', 'C', 'B', 'A', 'B', 'C']
-            assert next(iterator) == ['C', 'B', 'A', 'B', 'C', 'D']
-            assert next(iterator) == ['B', 'A', 'B', 'C', 'D', 'E']
+    for _ in range(3):
+        assert next(iterator) == ['A', 'B', 'C', 'D', 'E', 'F']
+
+        assert next(iterator) == ['B', 'C', 'D', 'E', 'F', 'E']
+        assert next(iterator) == ['C', 'D', 'E', 'F', 'E', 'D']
+        assert next(iterator) == ['D', 'E', 'F', 'E', 'D', 'C']
+        assert next(iterator) == ['E', 'F', 'E', 'D', 'C', 'B']
+
+        assert next(iterator) == ['F', 'E', 'D', 'C', 'B', 'A']
+
+        assert next(iterator) == ['E', 'D', 'C', 'B', 'A', 'B']
+        assert next(iterator) == ['D', 'C', 'B', 'A', 'B', 'C']
+        assert next(iterator) == ['C', 'B', 'A', 'B', 'C', 'D']
+        assert next(iterator) == ['B', 'A', 'B', 'C', 'D', 'E']
