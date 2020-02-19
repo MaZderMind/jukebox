5af295c04afead99bc96d94aaedd5e340e6d64e5
MaZderMind <git@mazdermind.de>
2020-02-19 11:10:06 +0100

    fix secondary leds in-situ

--- software/jukebox/secondary_leds.py
+++ software/jukebox/secondary_leds.py
@@ -11,11 +11,11 @@ COLORS = [(0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 
 class SecondaryLeds():
     def __init__(self, sender):
         self.sender = sender
-        self._stopped = False
+        self._stopped = True
 
     def start(self):
-        self._stopped = False
         print("starting secondary LEDs")
+        self._stopped = False
         asyncio.create_task(self.run())
 
     def stop(self):
@@ -25,13 +25,15 @@ class SecondaryLeds():
     def idle(self):
         print("idle secondary LEDs")
         self.sender.display_frame_on_secondary_leds((WHITE,) * N_LEDS)
+        self._stopped = True
 
     async def run(self):
         while True:
             for color in COLORS:
+                if self._stopped:
+                    self._stopped = False
+                    print("stopped secondary LEDs")
+                    return
+
                 self.sender.display_frame_on_secondary_leds((color,) * N_LEDS)
                 await asyncio.sleep(1)
-
-                if self._stopped:
-                    print("Stopped secondary LEDs")
-                    return
