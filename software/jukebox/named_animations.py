b46bb07ba17e524ea81eecb9545a1b88f01a7801
MaZderMind <git@mazdermind.de>
2021-07-18 20:45:57 +0200

    update named_animations

--- software/jukebox/named_animations.py
+++ software/jukebox/named_animations.py
@@ -1,10 +1,10 @@
 import random
 
-from animations import animation_solid
-from animations.animation_color_wipe import horizontal_wipe, vertical_wipe
-from animations.animation_meter import meter
-from animations.animation_pacman import pacman
-from animations.animation_stars import stars
+from animations.animation_color_wipe import VerticalWipe, HorizontalWipe
+from animations.animation_meter import Meter
+from animations.animation_pacman import Pacman
+from animations.animation_solid import Solid
+from animations.animation_stars import Stars
 from animations.color_utils import from_hex
 from animations.direction import Direction
 
@@ -21,42 +21,42 @@ rainbow = [
 
 def techno():
     return random.choice([
-        pacman(),
-        pacman(),
-        meter(),
-        meter(),
-        stars(500, colors=rainbow, speed=0.2),
-        vertical_wipe(rainbow, speed=0.5, direction=Direction.FORWARD),
-        vertical_wipe(rainbow, speed=0.5, direction=Direction.REVERSE),
-        horizontal_wipe(rainbow, speed=0.5, direction=Direction.FORWARD),
-        horizontal_wipe(rainbow, speed=0.5, direction=Direction.REVERSE),
+        Pacman(),
+        Pacman(),
+        Meter(),
+        Meter(),
+        Stars(500, colors=rainbow, speed=0.2),
+        VerticalWipe(rainbow, speed=0.5, direction=Direction.FORWARD),
+        VerticalWipe(rainbow, speed=0.5, direction=Direction.REVERSE),
+        HorizontalWipe(rainbow, speed=0.5, direction=Direction.FORWARD),
+        HorizontalWipe(rainbow, speed=0.5, direction=Direction.REVERSE),
     ])
 
 
 def slow():
     return random.choice([
-        stars(150, colors=rainbow, speed=0.01),
-        stars(150, colors=[(1, .4, 0)], speed=0.01),
-        stars(150, colors=[(0, 1, 0)], speed=0.01),
-        stars(150, colors=[(1, 0, 1)], speed=0.01),
-        stars(150, colors=[(0, 1, 1)], speed=0.01),
-        vertical_wipe([(1, 0, 0), (0, 0, 1)], speed=0.5, direction=Direction.FORWARD),
-        vertical_wipe([(0, 1, 0), (0, 0, 1)], speed=0.5, direction=Direction.REVERSE),
-        horizontal_wipe([(0, 1, 0), (1, 1, 0)], speed=0.5, direction=Direction.FORWARD),
-        horizontal_wipe(rainbow, speed=0.1, direction=Direction.REVERSE),
+        Stars(150, colors=rainbow, speed=0.01),
+        Stars(150, colors=[(1, .4, 0)], speed=0.01),
+        Stars(150, colors=[(0, 1, 0)], speed=0.01),
+        Stars(150, colors=[(1, 0, 1)], speed=0.01),
+        Stars(150, colors=[(0, 1, 1)], speed=0.01),
+        VerticalWipe([(1, 0, 0), (0, 0, 1)], speed=0.5, direction=Direction.FORWARD),
+        VerticalWipe([(0, 1, 0), (0, 0, 1)], speed=0.5, direction=Direction.REVERSE),
+        HorizontalWipe([(0, 1, 0), (1, 1, 0)], speed=0.5, direction=Direction.FORWARD),
+        HorizontalWipe(rainbow, speed=0.1, direction=Direction.REVERSE),
     ])
 
 
 def fast():
     return random.choice([
-        pacman(),
-        stars(175, colors=((.5, .5, 1), (1, .5, .5), (1, 1, .5),), speed=0.3),
-        horizontal_wipe((from_hex("000000"), from_hex("ffffff")), speed=0.95, direction=Direction.REVERSE),
-        horizontal_wipe([(1, 0, 0), (0, 0, 1)], speed=0.9, direction=Direction.FORWARD),
-        horizontal_wipe([(0, 1, 0), (0, 0, 1)], speed=0.9, direction=Direction.REVERSE),
-        vertical_wipe([(0, 1, 0), (1, 1, 0)], speed=0.9, direction=Direction.FORWARD),
-        vertical_wipe(rainbow, speed=0.5, direction=Direction.REVERSE),
-        vertical_wipe(rainbow, speed=0.5, direction=Direction.FORWARD),
+        Pacman(),
+        Stars(175, colors=((.5, .5, 1), (1, .5, .5), (1, 1, .5),), speed=0.3),
+        HorizontalWipe((from_hex("000000"), from_hex("ffffff")), speed=0.95, direction=Direction.REVERSE),
+        HorizontalWipe([(1, 0, 0), (0, 0, 1)], speed=0.9, direction=Direction.FORWARD),
+        HorizontalWipe([(0, 1, 0), (0, 0, 1)], speed=0.9, direction=Direction.REVERSE),
+        VerticalWipe([(0, 1, 0), (1, 1, 0)], speed=0.9, direction=Direction.FORWARD),
+        VerticalWipe(rainbow, speed=0.5, direction=Direction.REVERSE),
+        VerticalWipe(rainbow, speed=0.5, direction=Direction.FORWARD),
     ])
 
 
@@ -66,16 +66,16 @@ def country():
         from_hex('FFFF00'),  # yellow
     ]
     return random.choice([
-        stars(50, colors=[(1, .6, 0)], speed=0.01),
-        stars(150, colors=[(1, 1, 1)], speed=0.01),
-        stars(150, colors=[(1, 1, 1)], speed=0.2),
-        stars(150, colors=[(1, .4, 0)], speed=0.2),
-        vertical_wipe(colors, speed=0.25, direction=Direction.FORWARD),
-        vertical_wipe(colors, speed=0.25, direction=Direction.REVERSE),
-        horizontal_wipe(colors, speed=0.25, direction=Direction.FORWARD),
-        horizontal_wipe(colors, speed=0.25, direction=Direction.REVERSE),
+        Stars(50, colors=[(1, .6, 0)], speed=0.01),
+        Stars(150, colors=[(1, 1, 1)], speed=0.01),
+        Stars(150, colors=[(1, 1, 1)], speed=0.2),
+        Stars(150, colors=[(1, .4, 0)], speed=0.2),
+        VerticalWipe(colors, speed=0.25, direction=Direction.FORWARD),
+        VerticalWipe(colors, speed=0.25, direction=Direction.REVERSE),
+        HorizontalWipe(colors, speed=0.25, direction=Direction.FORWARD),
+        HorizontalWipe(colors, speed=0.25, direction=Direction.REVERSE),
     ])
 
 
 def solid(color):
-    return animation_solid.solid(from_hex(color))
+    return Solid(from_hex(color))
